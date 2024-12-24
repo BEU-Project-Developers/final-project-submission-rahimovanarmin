afc3deca0db2e95343024dbbce0e399664aca304
Nrmin <nerminrehimova004@gmail.com>
2024-12-24 23:16:51 +0400

    Added project files

---
+++ TaskSchudler/All Tasks.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskSchudler
+{
+    public partial class All_Tasks : Form
+    {
+        public All_Tasks()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void RemindersNav_Click(object sender, EventArgs e)
+        {
+            Reminders Obj = new Reminders();
+            Obj.Show();
+            this.Close();
+        }
+
+        private void ProfileNav_Click(object sender, EventArgs e)
+        {
+            Profile Object = new Profile();
+            Object.Show();
+            this.Close();
+        }
+
+        private void EscapeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ TaskSchudler/Profile.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskSchudler
+{
+    public partial class Profile : Form
+    {
+        public Profile()
+        {
+            InitializeComponent();
+        }
+
+        private void RemindersPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void RemindersNav_Click(object sender, EventArgs e)
+        {
+
+            Reminders Obj = new Reminders();
+            Obj.Show();
+            this.Close();
+        }
+
+        private void AllTasksNav_Click(object sender, EventArgs e)
+        {
+
+            All_Tasks Obj = new All_Tasks();
+            Obj.Show();
+            this.Close();
+        }
+
+        private void Logo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Logolabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ProfileNav_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EscapeButtonu_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
